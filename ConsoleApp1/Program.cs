793debf9040f0f223e254cff321ac75d37b19611
burak düz <burak_96_652@hotmail.com>
2020-02-18 10:26:44 +0300

    2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Jenkins!");
+            Console.WriteLine("Hello Jenkins2!");
         }
     }
 }
